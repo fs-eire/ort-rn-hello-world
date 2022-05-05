10f83a707591e42caf36c664e7c8a0895c7efb43
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2022-05-05 14:33:41 -0700

    reproduce crash.

--- App.tsx
+++ App.tsx
@@ -24,11 +24,31 @@ async function loadModel() {
   }
 }
 
+async function runModel() {
+  try {
+    const input0 = new Float32Array(28*28);
+    const feeds:Record<string, ort.Tensor> = {};
+
+    // this is the wrong dimension for input.
+    // the correct dim should be [1,28,28].
+    // the expected behavior for ORT is to throw the exception, however it crashes.
+    feeds[myModel.inputNames[0]] = new ort.Tensor(input0, [1,1,28,28]);
+    const results = await myModel.run(feeds);
+    Alert.alert(
+      'model run successfully',
+      `results: ${results}`);
+  } catch (e) {
+    Alert.alert('failed to run model', `${e}`);
+    throw e;
+  }
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>using ONNX Runtime for React Native</Text>
       <Button title='Load model' onPress={loadModel}></Button>
+      <Button title='Run' onPress={runModel}></Button>
       <StatusBar style="auto" />
     </View>
   );
