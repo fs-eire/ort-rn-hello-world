5d686d88c2720349f796abe3f5d8722bf1ffc0cc
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2022-05-10 15:05:37 -0700

    add code for inferencing

--- App.tsx
+++ App.tsx
@@ -24,11 +24,32 @@ async function loadModel() {
   }
 }
 
+async function runModel() {
+  try {
+    const inputData = new Float32Array(28 * 28);
+    const feeds:Record<string, ort.Tensor> = {};
+    feeds[myModel.inputNames[0]] = new ort.Tensor(inputData, [1, 28, 28]);
+    const fetches = await myModel.run(feeds);
+    const output = fetches[myModel.outputNames[0]];
+    if (!output) {
+      Alert.alert('failed to get output', `${myModel.outputNames[0]}`);
+    } else {
+      Alert.alert(
+        'model inference successfully',
+        `output shape: ${output.dims}, output data: ${output.data}`);
+    }
+  } catch (e) {
+    Alert.alert('failed to inference model', `${e}`);
+    throw e;
+  }
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>using ONNX Runtime for React Native</Text>
       <Button title='Load model' onPress={loadModel}></Button>
+      <Button title='Run' onPress={runModel}></Button>
       <StatusBar style="auto" />
     </View>
   );
